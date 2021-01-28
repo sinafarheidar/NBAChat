acd8576d54590251a17e22b9291575a04b4ca0ae
Sina Farheidar-Smith <sinafarheidar12@gmail.com>
2021-01-27 19:59:27 -0800

    Changed Design and added background

---
+++ client/src/components/Nav.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { signOut, isAuth } from './helpers';
+
+export default function Nav() {
+
+  const handleSignOut = () => {
+    signOut(() => {
+      console.log('Signing Out!')
+      window.location.href = '/signup'
+    })
+  }
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#051c2d'}}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to='/' style={{color: 'white'}}>NBA Chat</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {isAuth() ?
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <button onClick={handleSignOut} className="btn btn-outline-danger" aria-current="page">Sign Out</button>
+              </li>
+            </ul>
+          </div> : <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to='/signup' style={{color: 'white'}}>Sign Up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to='/signin' style={{color: 'white'}}>Sign In</Link>
+              </li>
+            </ul>
+          </div>}
+      </div>
+    </nav>
+  );
+}
